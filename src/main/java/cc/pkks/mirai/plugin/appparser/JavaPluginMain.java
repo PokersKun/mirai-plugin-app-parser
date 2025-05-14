45e6bc523c8c086805a7499f512b92be10ed589f
PokersKun <pocketfan233@gmail.com>
2025-05-14 16:38:11 +0800

    first commit.

--- src/main/java/cc/pkks/mirai/plugin/appparser/JavaPluginMain.java
+++ src/main/java/cc/pkks/mirai/plugin/appparser/JavaPluginMain.java
@@ -1,13 +1,8 @@
-package org.example.mirai.plugin;
+package cc.pkks.mirai.plugin.appparser;
 
-import kotlin.Lazy;
-import kotlin.LazyKt;
-import net.mamoe.mirai.console.permission.*;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
-import net.mamoe.mirai.contact.Member;
-import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
@@ -35,7 +30,7 @@ public final class JavaPluginMain extends JavaPlugin {
     public static final JavaPluginMain INSTANCE = new JavaPluginMain();
 
     private JavaPluginMain() {
-        super(new JvmPluginDescriptionBuilder("org.example.mirai-example", "0.1.0")
+        super(new JvmPluginDescriptionBuilder("cc.pkks.mirai.plugin.appparser", "0.1.0")
                 .info("EG")
                 .build());
     }
@@ -53,39 +48,6 @@ public final class JavaPluginMain extends JavaPlugin {
             //监听好友消息
             getLogger().info(f.getMessage().contentToString());
         });
-
-        myCustomPermission.getValue(); // 注册权限
-    }
-
-    // region mirai-console 权限系统示例
-    public static final Lazy<Permission> myCustomPermission = LazyKt.lazy(() -> {  // Lazy: Lazy 是必须的, console 不允许提前访问权限系统
-        // 注册一条权限节点 org.example.mirai-example:my-permission
-        // 并以 org.example.mirai-example:* 为父节点
-
-
-        // @param: parent: 父权限
-        //                 在 Console 内置权限系统中, 如果某人拥有父权限
-        //                 那么意味着此人也拥有该权限 (org.example.mirai-example:my-permission)
-        // @func: PermissionIdNamespace.permissionId: 根据插件 id 确定一条权限 id
-        try {
-            return PermissionService.getInstance().register(
-                    INSTANCE.permissionId("my-permission"),
-                    "一条自定义权限",
-                    INSTANCE.getParentPermission()
-            );
-        } catch (PermissionRegistryConflictException e) {
-            throw new RuntimeException(e);
-        }
-    });
-
-    public static boolean hasCustomPermission(User usr) {
-        PermitteeId pid;
-        if (usr instanceof Member) {
-            pid = new AbstractPermitteeId.ExactMember(((Member) usr).getGroup().getId(), usr.getId());
-        } else {
-            pid = new AbstractPermitteeId.ExactUser(usr.getId());
-        }
-        return PermissionService.hasPermission(pid, myCustomPermission.getValue());
     }
     // endregion
 }
